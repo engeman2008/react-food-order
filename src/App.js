bf96bf03e8b6ed8c64db5de464cb95b4db8314a6
Eman Mohammed <eman.cse2008@gmail.com>
2023-01-16 17:44:09 +0100

    finalize app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsSHown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCArtHAndler = () => {
+    setCartIsShown(false);
+  };
   return (
-    <Fragment>
-      <Header></Header>
+    <CartProvider>
+      {cartIsSHown && <Cart onClose={hideCArtHAndler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
